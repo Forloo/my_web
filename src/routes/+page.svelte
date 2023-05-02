259ba80d908671e4ebf705d3e55d3f2dc1fedb30
forloo <kiplingpower6@gmail.com>
2023-05-01 23:48:27 -0400

    pushing current

---
+++ src/routes/+page.svelte
@@ -0,0 +1,93 @@
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h7"
+          /></svg
+        >
+      </label>
+      <ul
+        tabindex="0"
+        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li><a>Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div class="navbar-end">
+    <button class="btn btn-ghost btn-circle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        /></svg
+      >
+    </button>
+    <button class="btn btn-ghost btn-circle">
+      <div class="indicator">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+          /></svg
+        >
+        <span class="badge badge-xs badge-primary indicator-item" />
+      </div>
+    </button>
+  </div>
+</div>
+<button class="btn bg-indigo-500">Button</button>
+
+<div class="container mx-auto border px-6">hello</div>
+
+<div class="artboard phone-1 bg-indigo-500 place-self-center">
+  320×80
+  <div class="overflow-y-scroll">
+    <button class="btn bg-indigo-500">Button</button>
+    <button class="btn bg-indigo-500">Button</button>
+    <button class="btn bg-indigo-500">Button</button>
+    <button class="btn bg-indigo-500">Button</button>
+  </div>
+</div>
+
+<div class="grid grid-cols-4 gap-4">
+  <div>01</div>
+  <!-- ... -->
+  <div>09</div>
+</div>
+
+<button
+  class="btn btn-success"
+  onclick="window.location.href='http://localhost:5173/enter_food'"
+>
+  Food recommender</button
+>
---
+++ src/routes/enter_food/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  let name = "";
+  let count = 0;
+
+  const incrementCounter = () => {
+    count += 1;
+  };
+  const handleInput = (event) => {
+    name = event.target.value;
+  };
+  const resetName = () => {
+    name = "Unkown";
+  };
+</script>
+
+<main>
+  <h1>Hello {name}</h1>
+  <input type="text" on:input={handleInput} />
+  <button on:click={resetName}>Set name to Unknown</button>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
